9be25d92e67a0e5f11e6c9f13ee898a252a54e4f
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-09-12 21:10:47 +0500

    class practice

---
+++ app.js
@@ -0,0 +1,183 @@
+// var a = 10, b = 25, c = 50, d = 35, e = 20;
+
+// document.write(`
+
+//     <br>
+
+//     <div class="dropdown">
+//   <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//     Dropdown
+//   </button>
+//   <ul class="dropdown-menu dropdown-menu-dark">
+//     <li><a class="dropdown-item active" href="#">Option 1</a></li>
+//     <li><a class="dropdown-item" href="#">Option 2</a></li>
+//     <li><a class="dropdown-item" href="#">Something else here</a></li>
+//     <li><hr class="dropdown-divider"></li>
+//     <li><a class="dropdown-item" href="#">Separated link</a></li>
+//   </ul>
+// </div>
+
+// `)
+
+
+
+
+// var city = prompt("Enter your city: ");
+
+// if ( city === "karachi" ) {
+
+//     alert("Welcome User..");
+
+// }
+
+// else {
+
+//     alert("You are not from karachi, please try again.")
+
+// }
+
+
+
+
+// var age = prompt("Enter your age!");
+
+// if ( age >= 18 ) {
+
+//     console.log("You are eligible for CNIC..")
+
+// }
+
+// else {
+
+//     console.log("You are not aligible for CNIC..")
+
+// }
+
+
+
+
+
+// var gender = prompt("Enter your gender");
+
+// var message;
+
+// if ( gender === "male" ) {
+
+//     message = "Welcome boy..";
+
+// }
+
+// else if ( gender === "female" ) {
+
+//     message = "Welcome sister..";
+
+// }
+
+// alert(message);
+
+// else {
+
+//     console.log("incorrect input..");
+
+// }
+
+
+
+
+// !== NOT OPERATOR   (Toggle)
+
+// var rollno = +prompt("Enter your rollno.");
+
+
+// if ( rollno === 500 ) {
+
+//     console.log("You are not allowed to sit in exam!");
+
+// }
+
+// else {
+
+//     console.log("Yiou are allowed to sit in exam!");
+
+// }
+
+// if ( rollno !== 500 ) {
+
+//     console.log("You are allowed to sit in exam!");
+
+// }
+
+// else {
+
+//     console.log("You are not allowed to sit in exam!");
+
+// }
+
+
+
+
+// var a = 100;
+
+// if(a){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// var a = undefined;
+
+// if(!!a){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+
+// var percentage = +prompt("Enter your percentage: ");
+
+
+// if (percentage <= 30) {
+
+//     console.log("FAIL!");
+
+// }
+
+// else if (percentage <= 50) {
+
+//     console.log("Your grade is D");
+
+// }
+
+// else if (percentage <= 60) {
+
+//     console.log("Your grade is C");
+
+// }
+
+// else if (percentage <= 70) {
+
+//     console.log("Your grade is B");
+
+// }
+
+// else if (percentage <= 79) {
+
+//     console.log("Your grade is A");
+
+// }
+
+// else if (percentage <= 100) {
+
+//     console.log("Your grade is A+");
+
+// }
+
+// else {
+
+//     console.log("invalid percentage!")
+
+// }
